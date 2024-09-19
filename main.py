fb17de9883cb1f622e86def578d49baacda3b428
rogerboroda <roger_boroda@mail.ru>
2024-09-19 18:38:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# string_info    - информация о строке
+# calls          - звонки
+# is_contains    - он содержит
+# list_to_search - список для поиска
+# count_calls    - считать звонки
+
+
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info (string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())  # длина строки, строка в верхнем и нижнем регистре.
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
+print(calls)
